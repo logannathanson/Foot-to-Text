d6a335bc2b3af91b5b3d84c088304c34e97e5b79
Nathanson <logannat@UMICH.EDU>
2017-02-05 17:07:31 -0500

    Example GUI

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.btnAddContact = new System.Windows.Forms.Button();
+            this.lstContacts = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(7, 4);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 68);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 120);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Phone";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(117, 4);
+            this.txtFirstName.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(189, 26);
+            this.txtFirstName.TabIndex = 3;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(117, 120);
+            this.txtPhone.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(189, 26);
+            this.txtPhone.TabIndex = 4;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(117, 68);
+            this.txtLastName.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(189, 26);
+            this.txtLastName.TabIndex = 5;
+            // 
+            // btnAddContact
+            // 
+            this.btnAddContact.Location = new System.Drawing.Point(316, 4);
+            this.btnAddContact.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.btnAddContact.Name = "btnAddContact";
+            this.btnAddContact.Size = new System.Drawing.Size(160, 76);
+            this.btnAddContact.TabIndex = 6;
+            this.btnAddContact.Text = "Add Contact";
+            this.btnAddContact.UseVisualStyleBackColor = true;
+            this.btnAddContact.Click += new System.EventHandler(this.btnAddContact_Click);
+            // 
+            // lstContacts
+            // 
+            this.lstContacts.FormattingEnabled = true;
+            this.lstContacts.ItemHeight = 20;
+            this.lstContacts.Location = new System.Drawing.Point(484, 4);
+            this.lstContacts.Name = "lstContacts";
+            this.lstContacts.Size = new System.Drawing.Size(122, 84);
+            this.lstContacts.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(284, 241);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(274, 39);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(284, 286);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(274, 38);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(284, 330);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(274, 34);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(284, 370);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(274, 37);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 577);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstContacts);
+            this.Controls.Add(this.btnAddContact);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Button btnAddContact;
+        private System.Windows.Forms.ListBox lstContacts;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private Contact[] phoneBook = new Contact[1];
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Write(Contact obj)
+        {
+            StreamWriter sw = new StreamWriter("Contacts.txt");
+            sw.WriteLine(phoneBook.Length + 1);
+            sw.WriteLine(obj.FirstName);
+            sw.WriteLine(obj.LastName);
+            sw.WriteLine(obj.Phone);
+
+            for (int i = 0; i < phoneBook.Length; ++i)
+            {
+                sw.WriteLine(phoneBook[i].FirstName);
+                sw.WriteLine(phoneBook[i].LastName);
+                sw.WriteLine(phoneBook[i].Phone);
+            }
+
+            sw.Close();
+        }
+
+        private void Read()
+        {
+            StreamReader sr = new StreamReader("Contacts.txt");
+            phoneBook = new Contact[Convert.ToInt32(sr.ReadLine())];
+
+            for (int i = 0; i < phoneBook.Length; ++i)
+            {
+                phoneBook[i] = new Contact();
+                phoneBook[i].FirstName = sr.ReadLine();
+                phoneBook[i].LastName = sr.ReadLine();
+                phoneBook[i].Phone = sr.ReadLine();
+            }
+
+            sr.Close();
+        }
+
+        private void Display()
+        {
+            lstContacts.Items.Clear();
+
+            for (int i = 0; i < phoneBook.Length; ++i)
+            {
+                lstContacts.Items.Add(phoneBook[i].ToString());
+            }
+            button1.Text = phoneBook[0].FirstName;
+            button2.Text = phoneBook[1].FirstName;
+            button3.Text = phoneBook[2].FirstName;
+            button4.Text = phoneBook[3].FirstName;
+        }
+
+        private void ClearForm()
+        {
+            txtFirstName.Text = String.Empty;
+            txtLastName.Text = String.Empty;
+            txtPhone.Text = String.Empty;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Read();
+            Display();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddContact_Click(object sender, EventArgs e)
+        {
+            Contact obj = new Contact();
+            obj.FirstName = txtFirstName.Text;
+            obj.LastName = txtLastName.Text;
+            obj.Phone = txtPhone.Text;
+
+            Write(obj);
+            Read();
+            Display();
+            ClearForm();
+
+        }
+    }
+}
